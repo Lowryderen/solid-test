aad3f947e4b5bf58e561db365d40067c1424fe3d
Lowryderen <lowryder@lowryderen.com>
2023-06-12 08:29:03 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,26 @@
-import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+import { createSignal } from "solid-js";
+import { Link } from "gatsby";
 
-export default function Home() {
+function HomePage() {
+  const [title] = createSignal("Shop AI Management");
+
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
+    <div style={{ backgroundColor: "#E6F2FF", minHeight: "100vh" }}>
+      <nav style={{ display: "flex", flexDirection: "column", padding: "20px" }}>
+        <Link to="/page1" style={{ marginBottom: "10px" }}>Button 1</Link>
+        <Link to="/page2" style={{ marginBottom: "10px" }}>Button 2</Link>
+        <Link to="/page3" style={{ marginBottom: "10px" }}>Button 3</Link>
+        <Link to="/page4" style={{ marginBottom: "10px" }}>Button 4</Link>
+        <Link to="/page5" style={{ marginBottom: "10px" }}>Button 5</Link>
+        <Link to="/page6" style={{ marginBottom: "10px" }}>Button 6</Link>
+      </nav>
+      <div style={{ padding: "20px" }}>
+        <h1>{title()}</h1>
+        {/* Add your page content here */}
+      </div>
+    </div>
   );
 }
+
+export default HomePage;
+
